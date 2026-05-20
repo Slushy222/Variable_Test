8d55efb39cba3e407bacbb28a6bbf30862841f2e
Taylor Sampson <taylorsampson101@gmail.com>
2026-05-20 01:33:17 -0400

    JS done, just css to finish

--- typeChange.js
+++ typeChange.js
@@ -1,34 +1,59 @@
-let selected_item = (document.querySelector(".scrollContainer")).getBoundingClientRect();
-let scroll_container = document.querySelector(".scroller");
-let primary_word = document.querySelectorAll(".primaryWord");
-scroll_container.scrollTo(0,selected_item.top - scroll_container.offsetHeight - selected_item.height);
-
-// let observer = new IntersectionObserver(entries => { 
-// 	entries.forEach(entry => {
-// 		with(entry) if(isIntersecting) target.children[0].checked = true;
-// 	});
-//   }, {root: scroll_container, rootMargin: `-51% 0px -49% 0px`});
-// document.querySelectorAll(`.scroller`).forEach(item => observer.observe(item));
-
-// if (selected_item == (children[0].checked == true)); {
-//     primary_word.classList.add(".hello");
-// };
-
 let typeEntries = (document.querySelectorAll(".typeEntry"))
+let firstType = (document.querySelector(".scroller > li:first-of-type"))
+let secondType = (document.querySelector(".scroller").children[1])
+let thirdType = (document.querySelector(".scroller").children[2])
+let fourthType = (document.querySelector(".scroller").children[3])
+let fifthType = (document.querySelector(".scroller").children[4])
+let sixthType = (document.querySelector(".scroller").children[5])
+let seventhType = (document.querySelector(".scroller").children[6])
+
+let heading1 = (document.querySelector("h1"))
 
 let observer = new IntersectionObserver(entries => {
-	console.log(entries)
+	// console.log(entries)
 	entries.forEach(entry => {
-		entry.target.classList.toggle("show", entry.isIntersecting)
+		entry.target.classList.toggle("selected", entry.isIntersecting)
 	})
-})
+	typeChange()
+},
+{
+	threshold: 0.75,
+}
+)
+
 typeEntries.forEach(typeEntry => {
 	observer.observe(typeEntry)
 })
 
 
-// observer.observe(typeEntries[0])
+function typeChange() {
+	if (firstType.classList.contains("selected")) {
+		heading1.classList.add("pressato")
+		heading1.classList.replace("cheee", "pressato")
 
-// if (observer.observe(typeEntries[0])) {
-// 	primary_word.classList.add(".pressato2");
-// };
+	} else if (secondType.classList.contains("selected")) {
+		heading1.classList.replace("pressato", "cheee")
+		heading1.classList.replace("firelli", "cheee")
+
+	} else if (thirdType.classList.contains("selected")) {
+		heading1.classList.replace("cheee", "firelli")
+		heading1.classList.replace("loretta", "firelli")
+
+	} else if (fourthType.classList.contains("selected")) {
+		heading1.classList.replace("firelli", "loretta")
+		heading1.classList.replace("meursault", "loretta")
+
+	} else if (fifthType.classList.contains("selected")) {
+		heading1.classList.replace("loretta", "meursault")
+		heading1.classList.replace("earlysans", "meursault")
+
+	} else if (sixthType.classList.contains("selected")) {
+		heading1.classList.replace("meursault", "earlysans")
+		heading1.classList.replace("scatterplot", "earlysans")
+
+	} else if (seventhType.classList.contains("selected")) {
+		heading1.classList.replace("earlysans", "scatterplot")
+	} else {
+		console.log("no type selected")
+	}
+}
